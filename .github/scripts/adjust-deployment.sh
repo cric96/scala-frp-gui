412a1934eea8de756daf1abce05f2d19cde00a7d
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-05-31 15:17:04 +0200

    fix name script

--- .github/scripts/adjust-deployment.sh
+++ .github/scripts/adjust-deployment.sh
@@ -3,6 +3,5 @@ mkdir ./public
 mkdir ./public/target
 mkdir ./public/target/scala-3.1.2
 
-cp ./js/target/scala-3.1.2/js-fastop.js ./public/target/scala-3.1.2/js-fastop.js
-
+cp ./js/target/scala-3.1.2/js-fastopt.js ./public/target/scala-3.1.2/js-fastopt.js
 cp ./js/index.html ./public/index.html
