6ed98c99ec0e44d40042399b64a360d5a4de1170
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-05-31 14:55:59 +0200

    add page deploy

---
+++ .github/scrips/adjust-deployment.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+mkdir ./public
+mkdir ./public/target
+mkdir ./public/target/scala-3.1.2
+
+cp ./js/target/scala-3.1.2/js-fastop.js ./public/target/scala-3.1.2/js-fastop.js
+
+cp ./js/index.html ./public/index.html
