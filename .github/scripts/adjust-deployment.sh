aae3b9ceec11171cb5bc4589214abf1d13127601
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2023-06-06 09:07:41 +0200

    fix: update scala version in adjust deployment

--- .github/scripts/adjust-deployment.sh
+++ .github/scripts/adjust-deployment.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 mkdir ./public
 mkdir ./public/target
-mkdir ./public/target/scala-3.1.2
+mkdir ./public/target/scala-3.2.2
 
-cp ./js/target/scala-3.1.2/js-fastopt.js ./public/target/scala-3.1.2/js-fastopt.js
+cp ./js/target/scala-3.2.2/js-fastopt.js ./public/target/scala-3.2.2/js-fastopt.js
 cp ./js/index.html ./public/index.html
